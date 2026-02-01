f6138c6b8a9df0171836fb31ac72e63a946d6e4e
packjuvenile <3283890195@qq.com>
2026-02-01 11:45:12 +0800

    modified:   python/core/options.py

--- python/core/options.py
+++ python/core/options.py
@@ -15,12 +15,14 @@ c.scrolling.smooth = True
 c.statusbar.padding = {"bottom": 1, "left": 3, "right": 3, "top": 1}
 c.statusbar.widgets = ["search_match", "text:|", "url", "text:|", "scroll", "text:|", "tabs"]
 
-c.tabs.padding = {"top": 3, "bottom": 3, "left": 3, "right": 3}
+c.tabs.padding = {"top": 5, "bottom": 5, "left": 1, "right": 0}
+c.tabs.position = "left"
+c.tabs.width = "5%"
 
 c.window.hide_decoration = True
 
-c.zoom.default = "80%"
-c.zoom.levels = ["20%", "30%", "40%", "50%", "60%","70%", "80%", "90%", "100%", "110", "120", "130", "140%", "150%", "160%", "170%", "180%", "190%", "200%"]
+c.zoom.default = "75%"
+c.zoom.levels = ["20%", "30%", "40%", "50%", "60%","70%","75%", "80%","85%", "90%", "100%", "110", "120", "130", "140%", "150%", "160%", "170%", "180%", "190%", "200%"]
 
 c.hints.padding = {"bottom": 1, "left": 1, "right": 1, "top": 1}
 c.hints.border = "none"
