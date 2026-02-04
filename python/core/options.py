9e1bf94a3bf4efa6e93c8a8b8573bf17fc46883c
packjuvenile <3283890195@qq.com>
2026-02-04 09:29:56 +0800

    modified:   .gitignore 	modified:   python/core/options.py 	new file:   quickmarks

--- python/core/options.py
+++ python/core/options.py
@@ -1,4 +1,5 @@
 c.fonts.default_size = "10pt"
+c.colors.webpage.preferred_color_scheme = "dark"
 
 c.content.headers.accept_language = "zh-CN,en-US"
 c.content.headers.user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36"
